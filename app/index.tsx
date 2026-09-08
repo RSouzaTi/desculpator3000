5ba7fc0db13f83b7ffc1340353958a33eb8959c5
Ricardo José de Souza <ti.ricardo.souza@gmail.com>
2026-09-08 17:54:59 -0300

    clear + styles

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,38 @@
-import { Text, View } from "react-native";
+import { geradorDesculpa } from "@/services/ai/generator";
+import { styles } from "@/styles";
+import { useState } from "react";
+import { Text, TextInput, TouchableOpacity, View } from "react-native";
 
 export default function Index() {
+  const [evento, setEvento] = useState("");
+  const [resposta, setResposta] = useState("");
+
+  const callDesculpa = async () => {
+    const desculpa = await geradorDesculpa(evento);
+    setResposta(desculpa);
+  };
+
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style={styles.container}>
+      <Text style={styles.titulo}>Desculpator 3000</Text>
+      <Text style={styles.subtitulo}>
+        sua maquina de desculpas profissional
+      </Text>
+
+      <TextInput
+        value={evento}
+        placeholder="Digite o evento que você quer evitar ... "
+        onChangeText={setEvento}
+        style={styles.input}
+      />
+      <TouchableOpacity style={styles.button} onPress={callDesculpa}>
+        <Text style={styles.buttonText}>Gerar Desculpa</Text>
+      </TouchableOpacity>
+
+      <View style={styles.card}>
+        <Text style={styles.cardTitle}>Sua Desculpa está pronta </Text>
+        <Text style={styles.cardText}>{resposta}</Text>
+      </View>
     </View>
   );
 }
---
+++ services/ai/generator.ts
@@ -0,0 +1,20 @@
+import { createGoogleGenerativeAI } from "@ai-sdk/google";
+import { generateText } from "ai";
+
+const google = createGoogleGenerativeAI({
+  apiKey: process.env.EXPO_PUBLIC_GEMINI_API_KEY,
+});
+
+export const geradorDesculpa = async (evento: string) => {
+  try {
+    const { text } = await generateText({
+      model: google("gemini-2.5-flash"),
+      system:
+        "Gere apenas uma desculpa engraçada. criativa e que não ofenda ninguém.",
+      prompt: evento,
+    });
+    return text;
+  } catch (error) {
+    return "Tenho que levar minha avó no Jiu-jitsu";
+  }
+};
